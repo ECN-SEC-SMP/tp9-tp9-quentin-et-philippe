19d9cef1af7d70cf4c65b8aec5b2dff21f19f0a0
simbadLM <quentin.balloy.pro@gmail.com>
2025-03-13 10:54:56 +0100

    commun

---
+++ EX2.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<string> chaines;
+chaines.push_back("Bonjour");
+chaines.push_back("Comment");
+chaines.push_back("Allez");
+chaines.push_back("Vous");
+chaines.push_back("?");
+
+
---
+++ ex1.cpp
@@ -0,0 +1,6 @@
+#include <vector>
+using namespace std;
+
+/* 1) */std::vector<int> tab;   
+/* 2) */std::vector<int> tab(3);
+/* 3) */
